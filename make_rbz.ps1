5b4a30d4aea72cc28d978d48914b51dc94a1b8a5
j v <vantjac@gmail.com>
2021-05-01 18:54:18 -0700

    make_rbz meant to be run from terminal

--- make_rbz.ps1
+++ make_rbz.ps1
@@ -2,5 +2,4 @@ Remove-Item su_extensions.rbz -ErrorAction SilentlyContinue
 Remove-Item su_extensions.zip -ErrorAction SilentlyContinue
 Compress-Archive -Path su_extensions,su_extensions.rb -DestinationPath su_extensions.zip
 Rename-Item su_extensions.zip su_extensions.rbz
-Write-Host "Success, press Enter to close."
-Read-Host
+Write-Host "Success :)"
