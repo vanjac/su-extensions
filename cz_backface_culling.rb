14ac0426de70a07b8ecdbdf1f044545e43fe59ce
j v <vantjac@gmail.com>
2021-05-21 20:25:02 -0700

    update extension info

--- cz_backface_culling.rb
+++ cz_backface_culling.rb
@@ -4,10 +4,10 @@ require 'extensions.rb'
 module Chroma
 
   unless file_loaded?(__FILE__)
-    ex = SketchupExtension.new('Hide Backfaces', 'cz_backface_culling/main')
+    ex = SketchupExtension.new('Hide Back Faces', 'cz_backface_culling/main')
     ex.description = 'Hide faces which face away from the camera'
-    ex.version = '-1'
-    ex.creator = 'vanjac'
+    ex.version = '1.0.0'
+    ex.creator = 'Jacob van\'t Hoog'
     Sketchup.register_extension(ex, true)
     file_loaded(__FILE__)
   end
