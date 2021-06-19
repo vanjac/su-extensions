600254d331e261ef90c8e9f7d31a4fc07e903dfd
j v <vantjac@gmail.com>
2021-06-18 19:00:20 -0700

    function to get selected schemas

--- cz_classify/main.rb
+++ cz_classify/main.rb
@@ -2,15 +2,30 @@ require 'sketchup.rb'
 
 module Chroma
 
+  def self.get_selected_schemas(selection)
+    schemas = {}
+    selection.each{ |entity|
+      if entity.is_a?(Sketchup::ComponentInstance) ||
+          entity.is_a?(Sketchup::Group)
+        schema_dict = entity.definition.attribute_dictionary(
+          "AppliedSchemaTypes")
+        if schema_dict
+          schema_dict.each_key{ |key| schemas[key] = true }
+        end
+      end
+    }
+    return schemas.keys
+  end
+
   unless file_loaded?(__FILE__)
     UI.menu.add_item('Add Classification') {
-      names = Sketchup.active_model.classifications.map {|c| c.name}
-      if names.length == 0
+      schemas = Sketchup.active_model.classifications.keys
+      if schemas.length == 0
         UI.messagebox("No classification schema loaded!", MB_OK)
         next
       end
-      result = UI.inputbox(["Schema", "Type"], [names[0], ""],
-        [names.join("|"), ""], "Add classification")
+      result = UI.inputbox(["Schema", "Type"], [schemas[0], ""],
+        [schemas.join("|"), ""], "Add classification")
       if !result
         next
       end
@@ -29,24 +44,13 @@ module Chroma
     }
 
     UI.menu.add_item('Remove Classification') {
-      schemas = {}
-      Sketchup.active_model.selection.each{ |entity|
-        if entity.is_a?(Sketchup::ComponentInstance) ||
-            entity.is_a?(Sketchup::Group)
-          schema_dict = entity.definition.attribute_dictionary(
-            "AppliedSchemaTypes")
-          if schema_dict
-            schema_dict.each_key{ |key| schemas[key] = true }
-          end
-        end
-      }
-      names = schemas.keys
-      if names.length == 0
+      schemas = self.get_selected_schemas(Sketchup.active_model.selection)
+      if schemas.length == 0
         UI.messagebox("No classifications for selected objects!", MB_OK)
         next
       end
-      result = UI.inputbox(["Schema"], [names[0]],
-        [names.join("|"), ""], "Remove classification")
+      result = UI.inputbox(["Schema"], [schemas[0]],
+        [schemas.join("|"), ""], "Remove classification")
       if !result
         next
       end
@@ -56,6 +60,16 @@ module Chroma
           entity.definition.remove_classification(result[0], "")
         end
       }
+    }
+
+    UI.add_context_menu_handler{ |context_menu|
+      schemas = self.get_selected_schemas(Sketchup.active_model.selection)
+      if schemas.length != 0
+        submenu = context_menu.add_submenu("Classifications")
+        schemas.each { |schema|
+          submenu.add_item(schema) { }
+        }
+      end
     }
   end
 end
