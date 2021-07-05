94987328697a845fd0274b23a682f91edbb5c10f
j v <vantjac@gmail.com>
2021-07-04 20:35:35 -0700

    state values are stored in animated objects instead of root

--- cz_states/state.rb
+++ cz_states/state.rb
@@ -1,88 +1,101 @@
 module Chroma
-  COMPONENT_STATES_DICT = "cz_states"
+  STATES_DICT = "cz_states"
 
   module ComponentState
     CURRENT_STATE_ATTR = "current"
-
-    # return definition, instance AttributeDictionaries
-    # either could be nil -- will not create if they don't exist!
-    def self.def_inst_state_collections(component)
-      def_dict = component.definition.attribute_dictionary(
-        COMPONENT_STATES_DICT)
-      inst_dict = component.attribute_dictionary(COMPONENT_STATES_DICT)
-      return (def_dict ? def_dict.attribute_dictionaries : nil),
-        (inst_dict ? inst_dict.attribute_dictionaries : nil)
-    end
 
     def self.get_state_list(component)
       if !component.is_a?(Sketchup::ComponentInstance) &&
           !component.is_a?(Sketchup::Group)
         return []
       end
-      def_state_dicts, inst_state_dicts = def_inst_state_collections(component)
-      return def_state_dicts ? def_state_dicts.map{ |d| d.name } : []
-    end
-
-    def self.get_current(component)
-      inst_state = component.get_attribute(
-        COMPONENT_STATES_DICT, CURRENT_STATE_ATTR)
-      if inst_state
-        return inst_state
+      def_states_dict = component.definition.attribute_dictionary(STATES_DICT)
+      if def_states_dict && def_states_dict.attribute_dictionaries
+        return def_states_dict.attribute_dictionaries.map{ |d| d.name }
       end
-      def_state = component.definition.get_attribute(
-        COMPONENT_STATES_DICT, CURRENT_STATE_ATTR)
-      return def_state
+      return []
     end
 
-    def self.set_current(component, state)
-      component.set_attribute(COMPONENT_STATES_DICT, CURRENT_STATE_ATTR, state)
-      component.definition.set_attribute(
-        COMPONENT_STATES_DICT, CURRENT_STATE_ATTR, state)
-    end
-
-    def self.set_state_in_place(component, state)
-      def_state_dicts, inst_state_dicts = def_inst_state_collections(component)
-      def_dict = def_state_dicts ? def_state_dicts[state] : nil
-      if def_dict
-        apply_state_dict(component, def_dict)
+    def self.set_state_list(component, states)
+      def_states_dict = component.definition.attribute_dictionary(STATES_DICT,
+        true)
+      # delete existing
+      if def_states_dict.attribute_dictionaries
+        def_states_dict.attribute_dictionaries.to_a.each{ |d|
+          def_states_dict.attribute_dictionaries.delete(d)
+        }
       end
-      inst_dict = inst_state_dicts ? inst_state_dicts[state] : nil
-      if inst_dict
-        apply_state_dict(component, inst_dict)
-      end
-      set_current(component, state)
+      states.each{ |state|
+        def_states_dict.set_attribute(state, "empty", 0)
+      }
+
+      update_states(component, states)
     end
 
-    def self.apply_state_dict(component, dict)
-      # TODO check if valid!!
-      dict.each{ |key, value|
-        prop = ComponentProp.from_key(key, component)
-        prop.set_value(value)
+    def self.update_states(component, states, update_state = nil)
+      states_set = Set.new(states)
+      iterate_in_groups(component) { |c|
+        inst_states_dict = c.attribute_dictionary(STATES_DICT)
+        if inst_states_dict && inst_states_dict.attribute_dictionaries
+          inst_states_dict.attribute_dictionaries.each{ |prop_dict|
+            value = nil
+            # add states
+            states.each{ |state|
+              if state == update_state || !prop_dict[state]
+                if value.nil?  # could be false
+                  value = ComponentProp.new(c, prop_dict.name).get_value
+                end
+                prop_dict[state] = value
+              end
+            }
+            # remove states; convert to array to delete while iterating
+            prop_dict.to_a.each{ |key, value|
+              if !states_set.include?(key)
+                prop_dict.delete_key(key)
+              end
+            }
+          }
+        end
       }
     end
 
-    def self.component_is_valid(c, root)
-      if !(c && c.valid? &&
-        (c.is_a?(Sketchup::ComponentInstance) || c.is_a?(Sketchup::Group)))
-        return false
-      end
-      if c == root
-        return true
-      end
-      # make sure c is a child of the root, or nested in unique groups (but not
-      # components, since those could exist elsewhere)
-      loop do
-        parent = c.parent
-        if parent == root.definition
-          return true
-        elsif parent.is_a?(Sketchup::ComponentDefinition) && parent.group? &&
-            parent.count_instances == 1
-          c = parent.instances[0]
-        else
-          return false
+    def self.get_state(component)
+      return component.get_attribute(STATES_DICT, CURRENT_STATE_ATTR) ||
+        component.definition.get_attribute(STATES_DICT, CURRENT_STATE_ATTR)
+    end
+
+    def self.set_state(component, state)
+      iterate_in_groups(component) { |c|
+        if c.is_a?(Sketchup::Group)
+          c.make_unique
         end
+
+        inst_states_dict = c.attribute_dictionary(STATES_DICT)
+        if inst_states_dict && inst_states_dict.attribute_dictionaries
+          inst_states_dict.attribute_dictionaries.each{ |prop_dict|
+            value = prop_dict[state]
+            if value
+              ComponentProp.new(c, prop_dict.name).set_value(value)
+            end
+          }
+        end
+      }
+
+      component.set_attribute(STATES_DICT, CURRENT_STATE_ATTR, state)
+      component.definition.set_attribute(STATES_DICT, CURRENT_STATE_ATTR, state)
+    end
+
+    def self.iterate_in_groups(component, is_root = true, &action)
+      action.call(component)
+
+      if is_root || component.is_a?(Sketchup::Group)
+        component.definition.entities.each{ |child|
+          if child.is_a?(Sketchup::ComponentInstance) ||
+              child.is_a?(Sketchup::Group)
+            iterate_in_groups(child, false, &action)
+          end
+        }
       end
-      return true
     end
   end
 end
