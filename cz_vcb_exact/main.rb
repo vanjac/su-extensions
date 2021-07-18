4a44ee348e92dbb50d18eed6deb2a9a7ba403f9a
j v <vantjac@gmail.com>
2021-07-18 14:55:58 -0700

    encode script once at start

--- cz_vcb_exact/main.rb
+++ cz_vcb_exact/main.rb
@@ -6,16 +6,34 @@ require 'shellwords'
 
 
 module Chroma
-  PLUGIN_PATH = "Plugins/cz_vcb_exact"
+  # the following is PowerShell code
+  READ_VCB_SCRIPT = %q{
+    using namespace System.Windows.Automation
+    Add-Type -AssemblyName UIAutomationClient
+    # TODO also UIAutomationTypes?
+
+    $focused = [AutomationElement]::FocusedElement
+    # search up until we find the window
+    # https://www.meziantou.net/detect-the-opening-of-a-new-window-in-csharp.htm
+    while ($focused.Current.ControlType -ne [ControlType]::Window) {
+      $focused = [TreeWalker]::RawViewWalker.GetParent($focused)
+      if ($null -eq $focused) {
+        exit 1  # bad focus
+      }
+    }
+    $findVCBEdit = New-Object PropertyCondition ([AutomationElement]::AutomationIdProperty), "24214"
+    $vcbEdit = $focused.FindFirst("Descendants", $findVCBEdit)
+    if ($null -eq $vcbEdit) {
+      exit 2  # can't find measurements toolbar
+    }
+    $vcbValue = $vcbEdit.Current.Name
+    Set-Content -Value $vcbValue -Path }  # file name goes here!
+
 
   def self.enter_exact_vcb
-    # https://stackoverflow.com/a/47639662
-    script = READ_VCB_SCRIPT + Shellwords.escape(@@vcb_temp_file.path)
-    encoded = Base64.strict_encode64(script.encode('utf-16le'))
-    command = "powershell.exe -encodedCommand #{encoded}"
     # run command WITHOUT showing window
     # https://www.ruby-forum.com/t/hiding-the-command-window-when-using-system-on-windows/75495/4
-    result = @@wsh.Run(command, 0, 1)
+    result = @@wsh.Run(@@get_vcb_command, 0, 1)
     if result != 0
       if result == 1
         UI.messagebox("Couldn't find SketchUp window (not focused)")
@@ -47,32 +65,13 @@ module Chroma
     # should be deleted when sketchup is closed
     @@vcb_temp_file = Tempfile.new('cz_vcb')
     @@vcb_temp_file.close
+    # https://stackoverflow.com/a/47639662
+    script = READ_VCB_SCRIPT + Shellwords.escape(@@vcb_temp_file.path)
+    encoded = Base64.strict_encode64(script.encode('utf-16le'))
+    @@get_vcb_command = "powershell.exe -encodedCommand #{encoded}"
 
     UI.menu.add_item('Enter Exact Measurement') {
       enter_exact_vcb
     }
   end
-
-  # the following is PowerShell code
-  READ_VCB_SCRIPT = %q{
-    using namespace System.Windows.Automation
-    Add-Type -AssemblyName UIAutomationClient
-    # TODO also UIAutomationTypes?
-
-    $focused = [AutomationElement]::FocusedElement
-    # search up until we find the window
-    # https://www.meziantou.net/detect-the-opening-of-a-new-window-in-csharp.htm
-    while ($focused.Current.ControlType -ne [ControlType]::Window) {
-      $focused = [TreeWalker]::RawViewWalker.GetParent($focused)
-      if ($null -eq $focused) {
-        exit 1  # bad focus
-      }
-    }
-    $findVCBEdit = New-Object PropertyCondition ([AutomationElement]::AutomationIdProperty), "24214"
-    $vcbEdit = $focused.FindFirst("Descendants", $findVCBEdit)
-    if ($null -eq $vcbEdit) {
-      exit 2  # can't find measurements toolbar
-    }
-    $vcbValue = $vcbEdit.Current.Name
-    Set-Content -Value $vcbValue -Path }  # file name goes here!
 end
