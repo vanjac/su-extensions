ab0211d809ed16be6ec17eca1572b582da91572d
j v <vantjac@gmail.com>
2021-07-18 11:33:45 -0700

    Hide Back Faces 1.0.2

--- cz_backface_culling.rb
+++ cz_backface_culling.rb
@@ -6,7 +6,7 @@ module Chroma
   unless file_loaded?(__FILE__)
     ex = SketchupExtension.new('Hide Back Faces', 'cz_backface_culling/main')
     ex.description = 'Hide faces which face away from the camera'
-    ex.version = '1.0.1'
+    ex.version = '1.0.2'
     ex.creator = 'Jacob van\'t Hoog'
     Sketchup.register_extension(ex, true)
     @@backface_culling_extension = ex
